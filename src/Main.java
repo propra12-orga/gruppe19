8d0724c5635f3ae480471825442669f4f5ff63b5
Hiep Dinh <trung.ding@hhu.de>
2012-05-10 17:51:17 +0200

    Das Teil erkennt jetzt Pfeiltasten und Mausklick+Position :D

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,75 @@
 import org.lwjgl.LWJGLException;
+import org.lwjgl.input.Keyboard;
+import org.lwjgl.input.Mouse;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.DisplayMode;
+import org.lwjgl.opengl.GL11;
 
 public class Main {
-	public void start() {
-		try {
-			Display.setTitle("Bomberman");
-			Display.setDisplayMode(new DisplayMode(640,480));
-			Display.create();
-		} catch (LWJGLException e) {
-			e.printStackTrace();
-			System.exit(0);
-		}
-		
-		
-		while (!Display.isCloseRequested()) {
-			
 
-			
-			Display.update();}
-		}
-		
-	
-	public static void main(String[] argv) {
-		Main displayExample = new Main();
-		displayExample.start();
+    public void start() {
+        try {
+        Display.setTitle("Bomberman");
+	    Display.setDisplayMode(new DisplayMode(640, 480));
+	    Display.create();
+	} catch (LWJGLException e) {
+	    e.printStackTrace();
+	    System.exit(0);
+	}      
+        
+        while (!Display.isCloseRequested()) {
+
+	    pollInput();
+	    Display.update();
 	}
-}
 
+	Display.destroy();
+    }
+
+    public void pollInput() {
+		
+        if (Mouse.isButtonDown(0)) {
+	    int x = Mouse.getX();
+	    int y = Mouse.getY();
+			
+	    System.out.println("Mausklick bei X: " + x + " Y: " + y);
+	}
+		
+		
+	while (Keyboard.next()) {
+	    if (Keyboard.getEventKeyState()) {
+	        if (Keyboard.getEventKey() == Keyboard.KEY_UP) {
+		    System.out.println("Up Key gedrueckt");
+		}
+		if (Keyboard.getEventKey() == Keyboard.KEY_DOWN) {
+		    System.out.println("Down Key gedrueckt");
+		}
+		if (Keyboard.getEventKey() == Keyboard.KEY_LEFT) {
+		    System.out.println("Left Key gedrueckt");
+		}
+		if (Keyboard.getEventKey() == Keyboard.KEY_RIGHT){
+			System.out.println("Right Key gedrueckt");
+		}
+	    } else {
+	        if (Keyboard.getEventKey() == Keyboard.KEY_UP) {
+		    System.out.println("Up Key losgelassen");
+	        }
+	    	if (Keyboard.getEventKey() == Keyboard.KEY_DOWN) {
+		    System.out.println("Down Key losgelassen");
+		    }
+		    if (Keyboard.getEventKey() == Keyboard.KEY_LEFT) {
+		    System.out.println("Left Key losgelassen");
+		    }
+		    if (Keyboard.getEventKey() == Keyboard.KEY_RIGHT){
+		    System.out.println("Right Key losgelassen");
+		    }
+	    }
+	}
+	
+    }
+
+    public static void main(String[] argv) {
+        Main game = new Main();
+	game.start();
+    }
+}
