0aa3500c0cb35e8d7c89caf9717f6ebe743016f6
Benjamin Schlueter <benjamin.schlueter@hhu.de>
2012-05-11 03:27:39 +0200

    Auflösung auf 640x480 geändert und neue Tilemap verwendet.

--- src/BombermanTest.java
+++ src/BombermanTest.java
@@ -7,38 +7,54 @@ import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.SpriteSheet;
 import org.newdawn.slick.tiled.TiledMap;
- 
+
 public class BombermanTest extends BasicGame {
-	private float playerX=20;
-	private float playerY=20;
-	private TiledMap map;	
+
+	private float playerX = 20;
+	private float playerY = 20;
+	private TiledMap map;
 	private Animation player;
+
 	public BombermanTest() {
 		super("BOMBASTISCHER MANN");
 	}
+
 	public void init(GameContainer container) throws SlickException {
 		container.setVSync(true);
-		SpriteSheet sheet = new SpriteSheet("res/bomberman1.png",32,32);
-		map = new TiledMap("res/map_ohne_alles.tmx");		
+		SpriteSheet sheet = new SpriteSheet("res/bomberman1.png", 32, 32);
+		map = new TiledMap("res/testmap2.tmx");
 		player = new Animation();
 		player.setAutoUpdate(true);
-		for (int frame=0;frame<3;frame++) {
-			player.addFrame(sheet.getSprite(frame,0), 150);
-		}		
+		for (int frame = 0; frame < 3; frame++) {
+			player.addFrame(sheet.getSprite(frame, 0), 150);
+		}
 	}
-	public void update(GameContainer container, int delta) { 
-		if (container.getInput().isKeyDown(Input.KEY_LEFT)) {playerX--;}
-		if (container.getInput().isKeyDown(Input.KEY_RIGHT)) {playerX++;}
-		if (container.getInput().isKeyDown(Input.KEY_UP)) {playerY--;}
-		if (container.getInput().isKeyDown(Input.KEY_DOWN)) {playerY++;}
+
+	// Steuerung (Very Basic!)
+
+	public void update(GameContainer container, int delta) {
+		if (container.getInput().isKeyDown(Input.KEY_LEFT)) {
+			playerX--;
+		}
+		if (container.getInput().isKeyDown(Input.KEY_RIGHT)) {
+			playerX++;
+		}
+		if (container.getInput().isKeyDown(Input.KEY_UP)) {
+			playerY--;
+		}
+		if (container.getInput().isKeyDown(Input.KEY_DOWN)) {
+			playerY++;
+		}
 	}
-	public void render(GameContainer container, Graphics g)  {
+
+	public void render(GameContainer container, Graphics g) {
 		map.render(0, 0);
 		g.drawAnimation(player, playerX, playerY);
 	}
+
 	public static void main(String[] argv) throws SlickException {
-		AppGameContainer container = 
-			new AppGameContainer(new BombermanTest(), 480, 480, false);
+		AppGameContainer container = new AppGameContainer(new BombermanTest(),
+				640, 480, true);
 		container.start();
 	}
 }
