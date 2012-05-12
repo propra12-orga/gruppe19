d318f6860c29c5ffd6521c4486ab3a1e45b4ea76
Hiep Dinh <trung.ding@hhu.de>
2012-05-12 19:56:55 +0200

    Feste Mauern sind da! Nur leider ist Bennys bessere Laufgeschwindigkeit dahin.. konnte das nich mit dem Poly synchen.

--- src/BombermanTest.java
+++ src/BombermanTest.java
@@ -7,13 +7,15 @@ import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.SpriteSheet;
 import org.newdawn.slick.tiled.TiledMap;
+import org.newdawn.slick.geom.Polygon;
 
 public class BombermanTest extends BasicGame {
 
-	private float playerX = 1;
-	private float playerY = 1;
-	private TiledMap map;
+	private float playerX = 32;
+	private float playerY = 32;
+	private BlockMap map;
 	private Animation player;
+	private Polygon playerPoly;
 	Boolean debug = true;				//True um zusätzliche Infos anzuzeigen.
 
 	public BombermanTest() {
@@ -23,30 +25,56 @@ public class BombermanTest extends BasicGame {
 	public void init(GameContainer container) throws SlickException {
 		container.setVSync(true);
 		SpriteSheet sheet = new SpriteSheet("res/bomberman1.png", 32, 32);
-		map = new TiledMap("res/testmap2.tmx");
+		map = new BlockMap("res/testmap2.tmx");
 		player = new Animation();
 		player.setAutoUpdate(true);
 		for (int frame = 0; frame < 3; frame++) {
 			player.addFrame(sheet.getSprite(frame, 0), 150);
 		}
+
+		playerPoly = new Polygon(new float[]{
+				playerX,playerY,
+				playerX+32,playerY,
+				playerX+32,playerY+32,
+				playerX,playerY+32
+		});
 	}
 
 	// Steuerung (Very Basic!)
 
-	public void update(GameContainer container, int delta) {
+	public void update(GameContainer container, int delta) throws SlickException {
 		if (container.getInput().isKeyDown(Input.KEY_LEFT)) {
 			playerX--;
+			playerPoly.setX(playerX);
+			if (entityCollisionWith()){
+				playerX++;
+				playerPoly.setX(playerX);
+			}
 		}
 		if (container.getInput().isKeyDown(Input.KEY_RIGHT)) {
 
 			playerX++;
-
+			playerPoly.setX(playerX);
+			if (entityCollisionWith()){
+				playerX--;
+				playerPoly.setX(playerX);
+			}
 		}
 		if (container.getInput().isKeyDown(Input.KEY_UP)) {
 			playerY--;
+			playerPoly.setY(playerY);
+			if (entityCollisionWith()){
+				playerY++;
+				playerPoly.setY(playerY);
+			}
 		}
 		if (container.getInput().isKeyDown(Input.KEY_DOWN)) {
 			playerY++;
+			playerPoly.setY(playerY);
+			if (entityCollisionWith()){
+				playerY--;
+				playerPoly.setY(playerY);
+			}
 		}
 		if (container.getInput().isKeyDown(Input.KEY_ESCAPE)) { // Esc beendet
 																// Spiel
@@ -54,15 +82,26 @@ public class BombermanTest extends BasicGame {
 		}
 	}
 
+	public boolean entityCollisionWith() throws SlickException {
+		for (int i = 0; i < BlockMap.entities.size(); i++) {
+			Block entity1 = (Block) BlockMap.entities.get(i);
+			if (playerPoly.intersects(entity1.poly)) {
+				return true;
+			}       
+		}       
+		return false;
+	}
+	
 	public void render(GameContainer container, Graphics g) {
-		map.render(0, 0);
-		g.drawAnimation(player, playerX * 32, playerY * 32);
+		BlockMap.tmap.render(0, 0);
+		g.drawAnimation(player, playerX, playerY);
+		g.draw(playerPoly);
 		if (debug) {g.drawString("X:" + playerX + " Y:" + playerY, 32, 460);}
 	}
 
 	public static void main(String[] argv) throws SlickException {
-		AppGameContainer container = new AppGameContainer(new BombermanTest(),
-				640, 480, true);
+		AppGameContainer container =
+	    new AppGameContainer(new BombermanTest(), 640, 480, false);
 		container.start();
 	}
 }
