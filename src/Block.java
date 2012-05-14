21a7b6088b907a813e79225288f84185b1c38df3
Hiep Dinh <trung.ding@hhu.de>
2012-05-14 18:11:09 +0200

    Kommentare zu den Block-Klassen hinzugefuegt.

--- src/Block.java
+++ src/Block.java
@@ -3,6 +3,11 @@ import org.newdawn.slick.geom.Polygon;
  
 public class Block  {
 	public Polygon poly;
+	
+	/* Der Mauer-Konstruktor "Block" nimmt die Koordinaten x, y, um zu wissen, wo er die Mauer baut.
+	 * Dann nimmt er die Eckpunkte unseres square-Arrays und weiß, wie groß die Mauer sein soll.
+	 */
+	
 	public Block(int x, int y, int test[],String type) {
         poly = new Polygon(new float[]{
 				x+test[0], y+test[1],
--- src/BlockMap.java
+++ src/BlockMap.java
@@ -6,9 +6,14 @@ public class BlockMap {
 	public static TiledMap tmap;
 	public static int mapWidth;
 	public static int mapHeight;
-	private int square[] = {0,0,30,0,30,30,0,30}; //square shaped tile
-	public static ArrayList<Object> entities;
+	private int square[] = {0,0,30,0,30,30,0,30}; //square shaped tile, Eckpunkte der Mauer
+	public static ArrayList<Object> entities; // Array für die Mauerstücke
  
+/* Nachfolgend wird die TiledMap analysiert. Unsere Map besteht zur Zeit aus zwei versch. Sprites,
+ * die jeweils Boden oder Mauer darstellen. Die tileID des Bodens ist 1, die der Mauer 17.
+ * Es wird bei jeder Map-Koordinate überprüft, ob sie der Mauer-ID entspricht. Wenn ja, wird sie als undurchlässig markiert.
+ */
+	
 	public BlockMap(String ref) throws SlickException {
 	
 		entities = new ArrayList<Object>();
@@ -21,7 +26,8 @@ public class BlockMap {
 				int tileID = tmap.getTileId(x, y, 0);
 				if (tileID == 17) {
 					entities.add(
-                                        new Block(x * 32, y * 32, square, "square")
+							// Nun wird die Mauer gemacht, in der Block-Klasse is geschrieben, wie es genau klappt
+                                        new Block(x * 32, y * 32, square, "square")                                
                                         );
 				}
 			}
