27f802b85c592aac12b5496a5f7b051a749d36c3
Hiep Dinh <trung.ding@hhu.de>
2012-05-23 10:56:16 +0200

    Hab mal ein Test-Package angelegt, um das Spiel in ein StateBasedGame umzuwandeln - dies ist besser, als das Menu-System, welches wir bisher haben und sollte auch das Neustart-Problem effektiv losen konnen. Aber bitte loest das Neustart-Problem mit dem bisherigen Menue! Danach koennen wir das StateBasedGame anfangen :)

---
+++ src/StateBasedGame/GameplayState.java
@@ -0,0 +1,41 @@
+package StateBasedGame;
+
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.BasicGameState;
+import org.newdawn.slick.state.StateBasedGame;
+
+/* 
+ * Wenn ich's richtig verstanden habe, wird das hier später der Spieleanteil von Bomberman
+ */
+
+public class GameplayState extends BasicGameState {
+ 
+    int stateID = 1; // 
+ 
+    GameplayState( int stateID ) 
+    {
+       this.stateID = stateID;
+    }
+ 
+    @Override
+    public int getID() {
+        return stateID;
+    }
+    @Override
+    public void init(GameContainer gc, StateBasedGame sb) throws SlickException {
+ 
+    }
+ 
+    public void render(GameContainer gc, StateBasedGame sb, Graphics g) throws SlickException {
+ 
+    }
+ 
+    public void update(GameContainer gc, StateBasedGame sb, int delta) throws SlickException {
+        
+        
+ 
+    }
+ 
+}
