8241c07d8e5863d8ba93a7810eb207c4cf0c5b65
Ilgar Bosatov <ilgar.bosatov@uni-duesseldorf.de>
2012-06-13 23:20:45 +0200

    Block nun wirklich fertig mit JavaDoc Kommentaren

--- src/de/game/bomberman/Block.java
+++ src/de/game/bomberman/Block.java
@@ -2,19 +2,26 @@ package de.game.bomberman;
 
 import org.newdawn.slick.*;
 
+/**
+ * @author Aenderungen durch Ilgar;
+ *  ### Die Klasse Block ist fuer die Erstellung der Bloecke in dem Spiel zustaendig. 
+ *  Hier stehen die Methoden und Konstruktoren, in denen in anderen Klassen drauf zugegriffen wird.
+ */
 public class Block extends SpielObjekt {
   
   private boolean zerstoerbar = false;
   
   /**
-   * @return the zerstoerbar
+   * @return zerstoerbar
+   * ## Methode untersucht ob ein Block zerstoerbar ist oder nicht.
    */
   public boolean isZerstoerbar() {
     return zerstoerbar;
   }
   
   /**
-   * @param zerstoerbar the zerstoerbar to set
+   * @param zerstoerbar zerstoerbar to set 
+   * ## Setzt Bloecke auf zerstoerbar.
    */
   public void setZerstoerbar(boolean zerstoerbar) {
     this.zerstoerbar = zerstoerbar;
@@ -23,6 +30,7 @@ public class Block extends SpielObjekt {
   /**
    * @param x Koordinate des Blocks
    * @param y Koordinate des Blocks
+   * ## Mit diesen Koordinaten wird die Grundmauer um das ganze Spiel gebaut.
    * @throws SlickException
    */
   public Block(int x, int y) throws SlickException {
@@ -32,6 +40,7 @@ public class Block extends SpielObjekt {
   /**
    * @param x Koordinate des Blocks
    * @param y Koordinate des Blocks
+   * ## Mit diesen Koordinaten werden die zerstoerbaren Bloecke gebaut. Der Boolean-typ markiert die zerstoerbaren Bloecke.
    * @param zerstoerbar ;prüft ob Block zerstoerbar ist
    * @throws SlickException
    */
@@ -40,12 +49,11 @@ public class Block extends SpielObjekt {
     this.zerstoerbar = zerstoerbar;
   }
   
-  /* (non-Javadoc)
+  /*
    * @see de.game.bomberman.SpielObjekt#draw(org.newdawn.slick.Graphics)
    */
   @Override
   public void draw(Graphics g) {
-    // TODO Auto-generated method stub
     
   }
   
