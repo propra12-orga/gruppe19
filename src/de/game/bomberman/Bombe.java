c836312ea2fdfd756c2b64435c2f4e0aea762ace
Ilgar Bosatov <ilgar.bosatov@uni-duesseldorf.de>
2012-06-13 23:41:15 +0200

    Bombe nun wirklich fertig mit JavaDoc Kommentaren

--- src/de/game/bomberman/Bombe.java
+++ src/de/game/bomberman/Bombe.java
@@ -2,6 +2,12 @@ package de.game.bomberman;
 
 import org.newdawn.slick.*;
 
+/**
+ * @author Ilgar (JavaDoc-Kommentare)
+ * ## Die Klasse Bombe ist fuer das Erstellen der Bombe im Spiel notwendig.
+ * Hier steht Image der Bombe, ihr Radius, die Eigenschaft, ob die Bombe explodiert ist oder nicht
+ * und der Zaehler der Bombe fuer die Explosion.
+ */
 public class Bombe extends SpielObjekt {
   
     //Image der Bombe
@@ -15,6 +21,8 @@ public class Bombe extends SpielObjekt {
   
   /**
    * @return the ExplodeRadius
+   * ## Der Explosionsradius prueft die Felder, wohin die Explosion sich ausbreiten kann
+   * und breitet sich dann in diese aus.
    */
   public int getExplodeRadius() {
     return ExplodeRadius;
@@ -22,6 +30,7 @@ public class Bombe extends SpielObjekt {
 
   /**
    * @param ExplodeRadius the explodeRadius to set
+   * ## Hier wird der Radius mit einem Setter versehen.
    */
   public void setExplodeRadius(int explodeRadius) {
     ExplodeRadius = explodeRadius;
@@ -31,6 +40,11 @@ public class Bombe extends SpielObjekt {
   /**
    * @param x Koordinate der Bombe
    * @param y Koordinate der Bombe
+   * ## Mit diesen Koordinaten wird die Bombe im Spiel plaziert. Weiterhin wird hier das Image der Bombe schonmal
+   * im imPath erwaehnt. Der Counter wird gleichzeitig auf 0 gesetzt.
+   * # In der draw() Methode wird dann das Image auf die Koordinaten x, y gesetzt, falls die Bombe nicht explodiert ist.
+   * # in der update() Methode wird dann der Zaehler der Bombe stetig erhoeht, bis 200, welches dann die Bombe mit dem Setter
+   * auf true setzt -- Explosion!
    * @throws SlickException
    */
   public Bombe(int x, int y) throws SlickException {
@@ -41,7 +55,7 @@ public class Bombe extends SpielObjekt {
   }
   
   @Override
-  /* (non-Javadoc)
+  /* 
    * @see de.game.bomberman.SpielObjekt#draw(org.newdawn.slick.Graphics)
    */
   public void draw(Graphics g) {
@@ -50,6 +64,8 @@ public class Bombe extends SpielObjekt {
 
   /**
    * @return explode
+   * ## Gibt explode zurueck, dh gibt false aus. "false" steht fuer neutral -- Bombe nicht explodiert.
+   * In der update() Methode wird dann der Setter, falls er aufgerufen wird, explode auf true setzen -- Explosion!
    */
   public boolean isExplode() {
     return explode;
@@ -57,13 +73,14 @@ public class Bombe extends SpielObjekt {
   
   /**
    * @param explode to set
+   * ## Der Setter fuer explode.
    */
   public void setExplode(boolean explode) {
     this.explode = explode;
   }
   
   @Override
-  /* (non-Javadoc)
+  /* 
    * @see de.game.bomberman.SpielObjekt#update(int)
    */
   public void update(int delta) throws SlickException {
