a7920bfe0158e13e173c25bb2e30b5c7ae615f29
Ilgar Bosatov <ilgar.bosatov@uni-duesseldorf.de>
2012-07-02 01:25:47 +0200

    Viktors Menu mit JavaDoc fertig gestellt und Kommentare umgestellt veraendert

--- src/de/game/bomberman/MainGame.java
+++ src/de/game/bomberman/MainGame.java
@@ -1,7 +1,4 @@
 
-   // Diese Klasse enthaelt die Main Methode und startet das Spiel in 800x600
-
-
 package de.game.bomberman;
 
 import org.newdawn.slick.AppGameContainer;
@@ -9,6 +6,10 @@ import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.*;
  
+/**
+ * Die Klasse "MainGame" beinhaltet die main-Methode und startet das Spiel.
+ * Hier wird das Spielfenster auf 800x600 eingestellt.
+ */
 public class MainGame extends StateBasedGame {
  
     public static final int MAINMENUSTATE          = 0;  
@@ -17,19 +18,31 @@ public class MainGame extends StateBasedGame {
     {
         super("BOMBERMAN");
  
-        this.addState(new MainMenuState(MAINMENUSTATE));   // State fuer das Menu hinzugefuegt
-        this.addState(new Bomberman(GAMEPLAYSTATE));       // State fuer das eigentliche Spiel hinzugefuegt
-        this.enterState(MAINMENUSTATE);                    // Das Menu wird als erstes aufgerufen
+     // State fuer das Menu hinzugefuegt
+        this.addState(new MainMenuState(MAINMENUSTATE));
+     // State fuer das eigentliche Spiel hinzugefuegt
+        this.addState(new Bomberman(GAMEPLAYSTATE));
+     // Das Menu wird als erstes aufgerufen
+        this.enterState(MAINMENUSTATE);
     }
  
+    /**
+     * @param args
+     * @throws SlickException
+     */
     public static void main(String[] args) throws SlickException
     {
          AppGameContainer app = new AppGameContainer(new MainGame());
  
-         app.setDisplayMode(800, 600, false); // Wird auf 800x600 im Fenstermodus gesetzt
+      // Wird auf 800x600 im Fenstermodus gesetzt
+         app.setDisplayMode(800, 600, false);
+      // starten des Spiels
          app.start();
     }
  
+    /*
+     * @see org.newdawn.slick.state.StateBasedGame#initStatesList(org.newdawn.slick.GameContainer)
+     */
     @Override
     public void initStatesList(GameContainer gameContainer) throws SlickException {
  
--- src/de/game/bomberman/MainMenuState.java
+++ src/de/game/bomberman/MainMenuState.java
@@ -8,21 +8,35 @@ import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
  
+/**
+ * Die Klasse "MainMenuState" ist fuer das Hauptmenue zustaendig.
+ * Hier wird die StateID fuer das Spiel gegeben und die Auswahlmoeglichkeiten 
+ * "Spiel starten" sowie "Ende" im Menu gezeigt.
+ * Außerdem erfolgt die Auswahl im Menu durch die Maus. Die Erkennung dieser 
+ * wird in dieser Klasse geschrieben.
+ */
 public class MainMenuState extends BasicGameState {
- 
-    int stateID = 0; // Die stateID fuer das Menu
+    
+   //Die stateID fuer das Menu
+    int stateID = 0; 
     
     Image background = null;
     Image startGameOption = null;
     Image exitOption = null;
     
-    float menuX = 0; // Position der Auswahlmöglichkeiten (Start und Ende) im Menü
+   // Position der Auswahlmöglichkeiten (Start und Ende) im Menü
+    float menuX = 0; 
     float menuY = 400;
     float scaleStep = 0.0001f;
  
     float startGameScale = 1;
     float exitScale = 1;
  
+    /**
+     * Ist fuer die ID des State zustaendig.
+     * MainMenuState wird ein Integer Wert gegeben.
+     * @param stateID
+     */
     MainMenuState( int stateID ) 
     {
        this.stateID = stateID;
@@ -33,77 +47,93 @@ public class MainMenuState extends BasicGameState {
         return stateID;
     }
  
+    /* 
+     * @see org.newdawn.slick.state.GameState#init(org.newdawn.slick.GameContainer, org.newdawn.slick.state.StateBasedGame)
+     */
+    
     public void init(GameContainer container, StateBasedGame sb) throws SlickException {
         
         // Menu-Bild laden und Auswahlbild deklarieren + laden
-        
         background = new Image("res/menubackground.jpg");
-        Image menuOptions = new Image("res/menuoptions.png"); // Zur Zeit ein Bild für Zwei Auswahlmöglichkeiten
+     
+       // Ein Bild für Zwei Auswahlmöglichkeiten
+        Image menuOptions = new Image("res/menuoptions.png");
          
-        startGameOption = menuOptions.getSubImage(0, 0, 377, 71); // Start-Option aus Auswahlbilds laden
+       // Start-Option aus Auswahlbilds laden
+        startGameOption = menuOptions.getSubImage(0, 0, 377, 71);
          
-        exitOption = menuOptions.getSubImage(0, 71, 377, 71); // Ende-Option aus Auswahbild laden
+       // Ende-Option aus Auswahbild laden
+        exitOption = menuOptions.getSubImage(0, 71, 377, 71); 
     }
  
+    /*
+     * @see org.newdawn.slick.state.GameState#render(org.newdawn.slick.GameContainer, org.newdawn.slick.state.StateBasedGame, org.newdawn.slick.Graphics)
+     */
     public void render(GameContainer container, StateBasedGame sb, Graphics gc1) throws SlickException {
-        // Hintergrund rendern und Variablen für die Auswahl deklarieren
+       // Hintergrund rendern und Variablen für die Auswahl deklarieren
         background.draw(0, 0);
-        // Auswahl-Bilder in die richtigen Positionen legen
+       // Auswahl-Bilder in die richtigen Positionen legen
         startGameOption.draw(menuX, menuY, startGameScale); 
         exitOption.draw(menuX, menuY+80, exitScale);
-        // Auswahl wird vergrößert, wenn Maus raufzeigt
+       // Auswahl wird vergrößert, wenn Maus draufzeigt
     }
  
+    
+    /*
+     * @see org.newdawn.slick.state.GameState#update(org.newdawn.slick.GameContainer, org.newdawn.slick.state.StateBasedGame, int)
+     */
     public void update(GameContainer container, StateBasedGame sb, int arg1) throws SlickException {
  
 
-Input input = container.getInput();
+      Input input = container.getInput();
  
-int mouseX = input.getMouseX();
-int mouseY = input.getMouseY();
+      int mouseX = input.getMouseX();
+      int mouseY = input.getMouseY();
  
-boolean insideStartGame = false;
-boolean insideExit = false;
+      boolean insideStartGame = false;
+      boolean insideExit = false;
  
-// Zeige der Maus, wo Start bzw. Ende Auswahl liegen
+     // Zeige der Maus, wo Start bzw. Ende Auswahl liegen
 
-if( ( mouseX >= menuX && mouseX <= menuX + startGameOption.getWidth()) &&
-    ( mouseY >= menuY && mouseY <= menuY + startGameOption.getHeight()) ){
-    insideStartGame = true;
-}else if( ( mouseX >= menuX && mouseX <= menuX+ exitOption.getWidth()) &&
-          ( mouseY >= menuY+80 && mouseY <= menuY+80 + exitOption.getHeight()) ){
-    insideExit = true;
-}
+        if( ( mouseX >= menuX && mouseX <= menuX + startGameOption.getWidth()) &&
+            ( mouseY >= menuY && mouseY <= menuY + startGameOption.getHeight()) ){
+            insideStartGame = true;
+              }
+        else if( ( mouseX >= menuX && mouseX <= menuX+ exitOption.getWidth()) &&
+                 ( mouseY >= menuY+80 && mouseY <= menuY+80 + exitOption.getHeight()) ){
+                   insideExit = true;
+              }
 
-// Mausklick führt zur Ausführung von Start bzw. Ende
-// !!! Zur Zeit schließt das Fenster auch dann, wenn man auf keine der beiden Optionen klickt !!!
+         // Mausklick führt zur Ausführung von Start bzw. Ende
+         // !!! Zur Zeit schließt das Fenster auch dann, wenn man auf keine der beiden Optionen klickt !!!
 
-if(insideStartGame){
-  if(startGameScale < 1.05f)
-    startGameScale += scaleStep * arg1;
+        if(insideStartGame){
+        if(startGameScale < 1.05f)
+          startGameScale += scaleStep * arg1;
  
-  if ( input.isMouseButtonDown(Input.MOUSE_LEFT_BUTTON) ){
+        if ( input.isMouseButtonDown(Input.MOUSE_LEFT_BUTTON) ){
     
-//bei Mausklick wird in den State 1 gewechselt, das eigentliche Spiel startet
+        //bei Mausklick wird in den State 1 gewechselt, das eigentliche Spiel startet
     
-    sb.enterState(1);              
-  }
-}else{
-  if(startGameScale > 1.0f)
-    startGameScale -= scaleStep * arg1;
+            sb.enterState(1);              
+            }
+        }else{
+          
+          if(startGameScale > 1.0f)
+            startGameScale -= scaleStep * arg1;
  
-  if ( input.isMouseButtonDown(Input.MOUSE_LEFT_BUTTON) )
-    container.exit();
-}
+          if ( input.isMouseButtonDown(Input.MOUSE_LEFT_BUTTON) )
+            container.exit();
+              }
  
-if(insideExit)
-{
-   if(exitScale < 1.05f)
-     exitScale +=  scaleStep * arg1;
-}else{
-  if(exitScale > 1.0f)
-    exitScale -= scaleStep * arg1;
-}
+          if(insideExit)
+         {
+       if(exitScale < 1.05f)
+         exitScale +=  scaleStep * arg1;
+       }else{
+         if(exitScale > 1.0f)
+           exitScale -= scaleStep * arg1;
+       }
         
     }
  
