cee4014814950fde4ba6fad9c4f1182035bd20fe
Dennis-Marco Fanty <dennis-marco.fanty@uni-duesseldorf.de>
2012-07-04 13:11:33 +0200

    Netztestgedoens, is aber noch nich viel...

---
+++ src/de/game/bomberman/tcp/Client.java
@@ -0,0 +1,41 @@
+import java.lang.*;
+import java.io.*;
+import java.net.*;
+
+class Client {
+   public static void main(String args[]) {
+      try {
+         Socket skt = new Socket("134.99.36.55", 1234);
+         BufferedReader in = new BufferedReader(new
+            InputStreamReader(skt.getInputStream()));
+         
+         
+         System.out.print("Received string: '");
+
+        
+            
+         
+         while(true){
+        
+             while (!in.ready()) {}
+            
+             
+             System.out.println(in.readLine()); // Read one line and output it
+
+            System.out.print("'\n");
+          
+          
+         
+             
+            //in.close();
+             
+      
+         }
+         
+        
+      }
+      catch(Exception e) {
+         System.out.print("Whoops! It didn't work!\n");
+      }
+   }
+}
