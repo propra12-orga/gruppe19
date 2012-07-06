4584e3a8e31ed25d3d46ba58edac0b8a23a68ce5
Ilgar Bosatov <ilgar.bosatov@uni-duesseldorf.de>
2012-07-06 20:02:32 +0200

    Doc bei MainGame drin

--- src/de/game/bomberman/MainGame.java
+++ src/de/game/bomberman/MainGame.java
@@ -3,12 +3,22 @@ package de.game.bomberman;
 import org.newdawn.slick.*;
 import org.newdawn.slick.state.*;
 
+/**
+ * Die Mainmethode in dieser Klasse gibt den Startschuss fuer das Spiel Bomberman.
+ * Es werden FPS eingestellt unter denen das Spiel laeuft und das Fenster eingestellt; 800x 600.
+ */
 public class MainGame extends StateBasedGame {
   
+  /**
+   * Das Spiel kann starten! Hier wird dem Konstruktor "Bomberman" übergeben.
+   */
   public MainGame() {
     super("Bomberman");
   }
   
+  /* 
+   * @see org.newdawn.slick.state.StateBasedGame#initStatesList(org.newdawn.slick.GameContainer)
+   */
   public void initStatesList(GameContainer container) {
     this.addState(new MainMenu());   // State fuer das Menu hinzugefuegt
     this.addState(new Tutorial());       // State fuer Tutorial hinzugefuegt
@@ -21,6 +31,14 @@ public class MainGame extends StateBasedGame {
     this.addState(new Credits());   // State fuer das Anzeigen der Credits
   }
   
+  /**
+   * Die Main Methode;
+   * Hier wird das Spiel aus aufgerufen und gestartet. 
+   * Die Frames per Second werden auf 58 gesetzt, das Display/ Fenster wird auf die Groesse 800x 600
+   * eingestellt und der Befehl das Spiel zu starten wird gegeben.
+   * 
+   * @param argv
+   */
   public static void main(String[] argv) {
     try {
       AppGameContainer container = new AppGameContainer(new MainGame());
